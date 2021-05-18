2a7e575ac0d970bf34a9fb28f3acb8bc58f82e77
Bukmop bacoB <BukmopbacoB@gmail.com>
2021-05-18 16:02:07 +0300

    Udemy 1.8 Dependency Injection by Setter

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -3,8 +3,13 @@ package spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    // pet -> setPet
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
 
