b25e6cc963495d5748207beaa2bec88cac151d73
Bukmop bacoB <BukmopbacoB@gmail.com>
2021-05-19 14:17:04 +0300

    Udemy 1.13 Methods: init and destroy

--- src/main/java/spring_introduction/Test4.java
+++ src/main/java/spring_introduction/Test4.java
@@ -7,17 +7,17 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Belka");
-        Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Strelka");
-
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
-
-        System.out.println("Bean is only one?  " + (myDog==yourDog));
-        System.out.println(myDog);
-        System.out.println(yourDog);
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        myDog.setName("Belka");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Strelka");
+//
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
+//
+//        System.out.println("Bean is only one?  " + (myDog==yourDog));
+//        System.out.println(myDog);
+//        System.out.println(yourDog);
 
         context.close();
     }
