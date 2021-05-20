d8a35423700666ebcfe973a56add928a0bbe039f
Bukmop bacoB <BukmopbacoB@gmail.com>
2021-05-20 17:44:22 +0300

    Udemy 1.18 DI by @Qualifier Annotations for field, method and constructor

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,27 +1,30 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
-    @Autowired
+//    @Autowired
+//    @Qualifier("dog")
     private Pet pet;
     private String surname;
     private int age;
 
-//    @Autowired
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
-
-    public Person() {
+    @Autowired
+    public Person(@Qualifier("dog") Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
     }
+
+//    public Person() {
+//        System.out.println("Person bean is created");
+//    }
 
     // pet -> setPet
 //    @Autowired
+//    @Qualifier("catBean")
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
